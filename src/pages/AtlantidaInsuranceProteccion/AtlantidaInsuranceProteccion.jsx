15f2493dba4a55e224cefd0e0c9724b101f6b587
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2024-10-08 14:38:47 -0600

    Proteccion integral terminada

--- src/pages/AtlantidaInsuranceProteccion/AtlantidaInsuranceProteccion.jsx
+++ src/pages/AtlantidaInsuranceProteccion/AtlantidaInsuranceProteccion.jsx
@@ -1,10 +1,11 @@
 import AtlantidaInsuranceProteccionHeader from "./components/AtlantidaInsuranceProteccionHeader";
 import AsistenciaInsurance from "../../components/AsistenciaInsurance";
 import SociosEstrategicos from "../../components/SociosEstrategicos";
+import subfooter from "../../images/salud-protegida-subfooter.jpeg";
 import InfoPrincipal from "../../components/InfoPrincipal";
-import subfooter from "../../images/subfooter30.jpg";
 import RedBanner from "../../components/RedBanner";
 import SubFooter from "../../components/SubFooter";
+import addUser from "../../images/add-user.png";
 
 const AtlantidaInsuranceProteccion = () => {
   return (
@@ -17,12 +18,12 @@ const AtlantidaInsuranceProteccion = () => {
 
       {/* Seguro de Vida */}
       <InfoPrincipal
-        iconClassnames={"w-56 mx-auto"}
+        iconClassnames={"w-20 mx-auto"}
         title={"Cobertura de Accidentes"}
         titleSpan={"Personales"}
         description={`Protégete ante los imprevistos cotidianos con el respaldo económico de los gastos generados,
         proporcionándole seguridad a tu familia.`}
-        icon={"https://dummyimage.com/600x400/000/fff"}
+        icon={addUser}
       />
 
       {/* Asistencia */}
@@ -39,7 +40,7 @@ const AtlantidaInsuranceProteccion = () => {
       <SubFooter
         bgColor={`black`}
         title={`Contigo, en cada momento importante`}
-        img={"https://dummyimage.com/600x400/000/fff"}
+        img={subfooter}
       />
     </>
   );
