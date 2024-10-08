cbc75d42d30ee42ceb87c3f7968085a3554ca3f7
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2024-10-08 14:16:31 -0600

    pagina conduce seguro terminada

--- src/pages/AtlantidaInsuranceConduceSeguro/AtlantidaInsuranceConduceSeguro.jsx
+++ src/pages/AtlantidaInsuranceConduceSeguro/AtlantidaInsuranceConduceSeguro.jsx
@@ -1,10 +1,11 @@
 import AtlantidaInsuranceViajaConfianzaHeader from "./components/AtlantidaInsuranceViajaConfianzaHeader";
 import AsistenciaInsurance from "../../components/AsistenciaInsurance";
 import SociosEstrategicos from "../../components/SociosEstrategicos";
+import subfooter from "../../images/conduce-seguro-subfooter.jpeg";
 import InfoPrincipal from "../../components/InfoPrincipal";
-import subfooter from "../../images/subfooter30.jpg";
-import RedBanner from "../../components/RedBanner";
 import SubFooter from "../../components/SubFooter";
+import RedBanner from "../../components/RedBanner";
+import car from "../../images/car.png";
 
 const AtlantidaInsuranceConduceSeguro = () => {
   return (
@@ -17,12 +18,12 @@ const AtlantidaInsuranceConduceSeguro = () => {
 
       {/* Seguro de Vida */}
       <InfoPrincipal
-        iconClassnames={"w-56 mx-auto"}
+        iconClassnames={"w-24 mx-auto"}
         title={"Cobertura de Vehículos"}
         description={`Cubre los riesgos asociados con la propiedad y el uso de un auto. Diseñado para ofrecerte tranquilidad al
         saber que, en caso de accidentes, robos, daños o cualquier otra eventualidad, contarás con el respaldo
         necesario para enfrentar las consecuencias financieras.`}
-        icon={"https://dummyimage.com/600x400/000/fff"}
+        icon={car}
       />
 
       {/* Asistencia */}
@@ -38,7 +39,7 @@ const AtlantidaInsuranceConduceSeguro = () => {
       <SubFooter
         bgColor={`black`}
         title={`Protección personalizada para tu bienestar`}
-        img={"https://dummyimage.com/600x400/000/fff"}
+        img={subfooter}
       />
     </>
   );
